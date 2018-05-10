129ca5a7642844d0fca5331d345360a9b19c2168
silver <admin@jishuz.cn>
2018-05-10 15:26:15 +0800

    new

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-from flask import Flask,render_template
+from flask import Flask, render_template, url_for
 import config
 
 app = Flask(__name__)
@@ -10,8 +10,15 @@ app.config.from_object(config)
 
 @app.route("/")
 def index():
-    return "ok"
+    return render_template("index.html")
 
+@app.route("/history/")
+def history():
+    return render_template("history.html")
+
+@app.route("/kadawo/")
+def kadawo():
+    return render_template("kadawo.html")
 
 if __name__ == '__main__':
     app.run()
