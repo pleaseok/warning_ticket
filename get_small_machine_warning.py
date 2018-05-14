0aa74615cb70c40d453263977a5045a65d838e2f
源 <1375643047@qq.com>
2018-05-14 18:58:19 +0800

    new

--- get_small_machine_warning.py
+++ get_small_machine_warning.py
@@ -11,6 +11,39 @@ class SmallWarning:
         self._loginUrl = r"http://www.kadawo.com/fulei/index.php/common/login/company/"
         self._url = r"http://www.kadawo.com/fulei/index.php/common/doLogin/company/"
 
+    def get_warning(self):
+        username = 'fulei'
+        password = 'bdxl88888*'
+
+    def get_login(self):
+        header = {'Host': 'www.kadawo.com',
+                  'Proxy-Connection': 'keep-alive',
+                  'pgrade-Insecure-Requests': '1',
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                'Chrome/66.0.3359.117 Safari/537.36',
+                  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                  'Accept-Encoding': 'gzip, deflate',
+                  'Accept-Language': 'zh-CN,zh;q=0.9'
+                  }
+        return header
+
+    def post_login(self):
+        header = {'Host': 'www.kadawo.com',
+                  'Proxy-Connection': 'keep-alive',
+                  'Content-Length': '84',
+                  'Cache-Control': 'max-age=0',
+                  'Origin': 'http://www.kadawo.com',
+                  'Upgrade-Insecure-Requests': '1',
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                'Chrome/66.0.3359.117 Safari/537.36',
+                  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                  'Referer': 'http://www.kadawo.com/fulei/index.php/common/login/company/',
+                  'Accept-Encoding': 'gzip, deflate',
+                  'Accept-Language': 'zh-CN,zh;q=0.9'
+                  }
+        return header
+
     def doLogin(self):
         username = 'fulei'
         password = 'bdxl88888*'
